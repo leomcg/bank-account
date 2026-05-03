33a128dcefa40db46359025b1fb5c5a4bc0b4867
leomcg <leomcg@gmail.com>
2026-05-03 11:21:58 -0300

    deposit and withdrawal functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useReducer, useState } from "react";
+
+const initialState = {
+  isAccountOpen: false,
+  balance: 0,
+  loan: 0,
+  isLoanActive: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "openAccount":
+      return { ...state, isAccountOpen: true };
+    case "closeAccount":
+      return { ...initialState };
+    case "deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "withdrawal":
+      return { ...state, balance: state.balance - action.payload };
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [{ isAccountOpen, balance, loan, isLoanActive }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  const [deposit, setDeposit] = useState(0);
+  const [withdrawal, setWithdrawal] = useState(0);
+
+  const closeAccount = () => {
+    setDeposit(0);
+    setWithdrawal(0);
+    dispatch({ type: "closeAccount" });
+  };
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      {isAccountOpen && (
+        <>
+          <div>
+            <p>Balance: {balance}</p>
+            <p>Loan: {loan}</p>
+          </div>
+          <div>
+            <input
+              type="text"
+              value={deposit}
+              onChange={(e) => setDeposit(Number(e.target.value))}
+            />
+            <button
+              onClick={() => dispatch({ type: "deposit", payload: deposit })}
+            >
+              Deposit $
+            </button>
+          </div>
+          <div>
+            <input
+              type="text"
+              value={withdrawal}
+              onChange={(e) => setWithdrawal(Number(e.target.value))}
+            />
+            <button
+              onClick={() =>
+                dispatch({ type: "withdrawal", payload: withdrawal })
+              }
+            >
+              Withdraw $
+            </button>
+          </div>
+        </>
+      )}
 
-      <div className="ticks"></div>
+      <button onClick={() => dispatch({ type: "openAccount" })}>
+        Open Account
+      </button>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <button onClick={closeAccount}>Close Account</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
